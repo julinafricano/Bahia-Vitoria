cf9bb9e267a2e70bc9674842dc2d5302296903a0
GABRIEL YUJI KIYOTA <20211170150051@ifrn.local>
2023-12-29 17:40:44 -0300

    começando player

--- WarBox/Assets/scripts/player.cs
+++ WarBox/Assets/scripts/player.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int vida = 3;
+    public float velocidade = 5.0f;
+    private float tempoA;
+    private float tempoB;
+    public Animator anim;
+    private float tempoMortal;
+  
+    public SpriteRenderer sprite;
+    public bool morreu;
+    public bool ultimoMoveA;
 
-    // Update is called once per frame
+    public bool ultimoMoveD;
+
+    public bool matouBoss1;
+    public bool matouBoss2;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        velocidade = 5f;
+    }
     void Update()
     {
-        
+        tempoMortal = tempoMortal + Time.deltaTime;
+        tempoA = tempoA + Time.deltaTime;
+        tempoB = tempoB + Time.deltaTime;
+
+        float movimentoHorizontal = Input.GetAxis("Horizontal");
+        float movimentoVertical = Input.GetAxis("Vertical");
+
+        Vector3 movimento = new Vector3(movimentoHorizontal, movimentoVertical, 0.0f);
+
+        movimento.Normalize();
+
+        transform.Translate(movimento * velocidade * Time.deltaTime);
+
     }
+
 }
